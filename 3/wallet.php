86942c6ab01e00bfd08f14a4c390ff22517fcabd
armadi21 <45292106+armadi21@users.noreply.github.com>
2018-11-30 18:20:32 +0800

    Add files via upload

---
+++ 3/wallet.php
@@ -0,0 +1,18 @@
+<?php
+
+//* @SCRIPT AUTO PAYOUT BITCOIN
+//* @AKBAR FEAT IPKZONE ( IDDANT )
+//* @TEAM : CRYPTO - TUYUL.CO.ID
+
+date_default_timezone_set('Asia/Jakarta');
+error_reporting(true);
+set_time_limit(0);
+ini_set('memory_limit', '-1');
+ini_set('output_buffering',0); 
+ini_set('request_order', 'GP');
+ini_set('variables_order','EGPCS');
+ini_set('max_execution_time','-1');
+
+// INPUT YOUR WALLET
+$argv[1] = "LPnSSTNBnkqJnAAjucpSqt2hC3TkMV1x2Z";
+?>
